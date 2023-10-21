928881ca2b655c982af99803ee2307c7c5df5251
Giorgi <gioruxa6@gmail.com>
2023-10-21 13:22:17 +0400

    Add project files.

---
+++ G12_2023_10_14/MyLinq.cs
@@ -0,0 +1,67 @@
+﻿namespace G12_2023_10_14.Linq
+{
+	public static class MyLinq
+	{
+		public static IEnumerable<T> MyWhere<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+		{
+			if (source == null) throw new ArgumentNullException(nameof(source));
+			if (predicate == null) throw new ArgumentNullException(nameof(predicate));
+
+			foreach (var item in source)
+			{
+				if (predicate(item))
+				{
+					yield return item;
+				}
+			}
+		}
+
+		public static bool MyAny<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+		{
+			if (source == null) throw new ArgumentNullException(nameof(source));
+			if (predicate == null) throw new ArgumentNullException(nameof(predicate));
+
+			foreach (var item in source)
+			{
+				if (predicate(item))
+				{
+					 return true;
+				}
+			}
+
+			return false;
+		}
+
+		public static bool MyAll<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+		{
+			int count = 0;
+			if (source == null) throw new ArgumentNullException(nameof(source));
+			if (predicate == null) throw new ArgumentNullException(nameof(predicate));
+
+			foreach (var item in source)
+			{
+				if (predicate(item))
+				{
+					count++;
+				}
+			}
+
+			return count == source.Count();
+		}
+
+		public static IEnumerable<TResult> MySelect<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector)
+		{
+			if (source == null) throw new ArgumentNullException(nameof(source));
+			if (selector == null) throw new ArgumentNullException(nameof(selector));
+
+			var result = new List<TResult>();
+
+            foreach (var item in source)
+            {
+				result.Add(selector(item));
+            }
+
+			return result;
+		}
+	}
+}
---
+++ G12_2023_10_14/Program.cs
@@ -0,0 +1,13 @@
+﻿using G12_2023_10_14.Linq;
+
+namespace G12_2023_10_14
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var myList = Employee.GetAllEmployees();
+			var myIenumerable = myList.All(x => x.ID > 5);
+		}
+	}
+}
